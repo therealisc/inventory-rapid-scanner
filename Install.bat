b05f8b36ce6ef3a0585894035d31c7cb03a12a24
Ioan Scafa <85192263+therealisc@users.noreply.github.com>
2023-10-18 09:59:51 +0300

    Set registry for vfpoledb

--- Install.bat
+++ Install.bat
@@ -3,8 +3,13 @@ winget install Microsoft.DotNet.SDK.7
 
 cd /
 git clone https://github.com/therealisc/inventory-rapid-scanner.git
-
+cd inventory-rapid-scanner
 git pull
 
-cd inventory-rapid-scanner/ConsoleScanner
-dotnet run
+copy vfpoledb.dll c:\Windows\System32
+cd c:\Windows\System32
+%windir%\System32\regsvr32.exe /u Vfpoledb.dll
+%windir%\System32\regsvr32.exe /i Vfpoledb.dll
+
+%~dp0Launch.bat
+pause
