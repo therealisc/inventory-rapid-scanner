6e7141c51715e177d9e75c4181559711a705286f
therealisc <therealisc@proton.me>
2024-08-21 07:36:41 -0400

    Change barcode type to decimal

--- DScannerLibrary/Models/ArticleModel.cs
+++ DScannerLibrary/Models/ArticleModel.cs
@@ -6,7 +6,7 @@ public class ArticleModel
     public string? denumire { get; set; }
     public decimal tva { get; set; }
     public decimal pret_vanz { get; set; }
-    public string cod_bare { get; set; }
+    public decimal? cod_bare { get; set; }
     //public string? lot { get; set; }
     public string? data_intr { get; set; }
 }
