665bebc09347d950f7c1d887ba9b8079798201b9
therealisc <85192263+therealisc@users.noreply.github.com>
2026-06-15 12:58:40 +0300

    Update DatabaseDirectoryHelper.cs

--- DScannerLibrary/Helpers/DatabaseDirectoryHelper.cs
+++ DScannerLibrary/Helpers/DatabaseDirectoryHelper.cs
@@ -46,11 +46,11 @@ public static class DatabaseDirectoryHelper
 		if (isLinux == false && !Directory.Exists($"{rootDirectory}/{sagaDirectoryName}"))
 		{
 			rootDirectory = Environment.CurrentDirectory;
-			Directory.CreateDirectory($"{rootDirectory}/{sagaDirectoryName}/1000");
+			sagaDirectoryName = "saga";
+			//Directory.CreateDirectory($"{rootDirectory}/{sagaDirectoryName}/1000");
 		}
 
 	    var sagaDirectory = SearchDirectory(rootDirectory, sagaDirectoryName, 0);
-
 		if (string.IsNullOrEmpty(sagaDirectory))
 		    continue;
 
