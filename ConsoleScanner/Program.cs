2833f14fe85bba73f8a2a4a0577d2af4b98b6c9c
therealisc <85192263+therealisc@users.noreply.github.com>
2026-07-02 03:52:50 +0300

    Add Barcode field to database and data processing

--- ConsoleScanner/Program.cs
+++ ConsoleScanner/Program.cs
@@ -22,10 +22,12 @@ var tableName = "articole";
 var dbfLines = dbfDataAccess.ReadDbf($"{ tableName }.dbf");
 
 string itemName = "";
+string itemBarcode = "";
 
 var sql = $@"CREATE TABLE {tableName} (
 		Id int PRIMARY KEY,
-		Denumire varchar(255) NOT NULL
+		Name varchar(255) NOT NULL,
+		Barcode varchar(255) NOT NULL
 		)";
 
 dataAccess.InsertData(sql);
@@ -35,16 +37,20 @@ foreach (var dbfLine in dbfLines)
 {
 	var lineSplit = dbfLine.ToString().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
 	var nameArray = lineSplit[1].ToString().Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+	var barcodeArray = lineSplit[15].ToString().Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
 
-	itemName = nameArray[1];	
+	itemName = nameArray[1];
+	itemBarcode = barcodeArray[1];
+	
 	Console.WriteLine(itemName);
+	Console.WriteLine(itemBarcode);
 	
 	//var uniqueId = Guid.NewGuid();
 	counter++;
 	var numberId = $"{counter}";
 
 	// the sql db converted varchar to int down below 
-	sql = $@"INSERT INTO { tableName } (Id, Denumire) VALUES ({ numberId }, '{ itemName }' )";
+	sql = $@"INSERT INTO { tableName } (Id, Name, Barcode) VALUES ({ numberId }, '{ itemName }', '{ itemBarcode }' )";
 	dataAccess.InsertData(sql);
 }
 
@@ -52,6 +58,6 @@ Console.WriteLine("--- Rows available ---");
 sql = $@"SELECT * FROM { tableName }";
 
 var entries = dataAccess.ReadData<DisplayModel>(sql);
-entries.ForEach(d => Console.WriteLine($"{ d.Id } { d.Denumire }"));
+entries.ForEach(d => Console.WriteLine($"{ d.Id } { d.Name } { d.Barcode }"));
 
 return;
