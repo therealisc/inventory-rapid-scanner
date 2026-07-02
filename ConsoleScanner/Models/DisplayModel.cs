c02a3f3c612efb19d016e29b4bd76cdfa6f038a2
therealisc <85192263+therealisc@users.noreply.github.com>
2026-07-02 03:52:27 +0300

    Rename Denumire to Name and add Barcode property

--- ConsoleScanner/Models/DisplayModel.cs
+++ ConsoleScanner/Models/DisplayModel.cs
@@ -3,5 +3,6 @@ namespace ConsoleScanner.Models;
 public class DisplayModel
 {
     public long Id { get; set; }
-    public string Denumire { get; set; }
+    public string Name { get; set; }
+    public string Barcode { get; set; }
 }
