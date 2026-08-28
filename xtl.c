538cbbcb275d054e252cd7636d0b582606929883
ioansc <therealisc@proton.me>
2026-08-28 13:36:58 +0000

    New interface testing

---
+++ xtl.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//#include <sql.h>
+
+
+int main() {
+        int dec = 0;
+	int bit = 1;
+	int n = 10000000;
+
+	while (n) {
+		int last_digit = n % 10;
+		n = n / 10;
+		dec += last_digit * bit;
+		bit = bit * 2;
+	}
+	    
+	printf("%d\n", dec);
+	printf("%d\n", dec);
+
+        return dec;
+}
